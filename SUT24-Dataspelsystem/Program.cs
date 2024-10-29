535ceb7f4b8e18767de8622f76858a7b7f2e6441
FelloMan <filip0816@hotmail.com>
2024-10-29 14:55:12 +0100

    Add project files.

---
+++ SUT24-Dataspelsystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SUT24_Dataspelsystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
